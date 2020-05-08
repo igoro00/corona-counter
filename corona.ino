687a03da4ffcbf5462755350bea1a7638d024430
igoro00 <igor.ordecha@gmail.com>
2020-05-08 22:58:39 +0200

    Code cleanup nie testowane, ale myśle że parseInt jest tu nie potrzebny. Wysyłasz i tak tylko chary cyfr wiec możesz po prostu printować je bezpośrednio. Again, nie testowane bo nie mam wolnego ekranu i arduino.

--- corona.ino
+++ corona.ino
@@ -1,27 +1,29 @@
+#define debug true
+
 // include the library code:
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
- int licznik = 0;
+char incomingByte = 0;
 void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
-  lcd.clear();
-  lcd.print("in Poland");
 }
 
 void loop() {
-  lcd.setCursor(0, 1);
-  if (Serial.available() > 2) {
-    int ile = Serial.parseInt();
-    Serial.print("Wczytalam ");
-          lcd.print(ile);
-          Serial.print(ile);
-          licznik ++;
-  }
-  if(licznik > 10){
+  if(Serial.available()>0){
+    //if there's a new message
+    
+    lcd.setCursor(0, 1);
     lcd.clear();
-      lcd.print("In Poland");
-Serial.print("wyczyściłem");
-licznik = 0;
+    lcd.print("Infected in Poland:");
+    while (Serial.available()>0) {
+      //keep recieving the message until the entire message is read
+      
+      incomingByte = Serial.read();
+      if(debug){
+        Serial.println("Recieved: " + incomingByte);
+      }
+      lcd.print(incomingByte);
+    }
   }
 }
